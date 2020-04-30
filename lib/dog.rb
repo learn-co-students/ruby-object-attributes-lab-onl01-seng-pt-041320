e6f0d1c3656010a7812e2675cf3401cc1ad44062
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-30 13:12:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+  def name= (fido_name)
+    @name = fido_name
+  end
+  def name= (fido_name)
+    @name = "Fido"
+  end
+  def breed= breed
+    @breed = breed
+  end
+
+  def breed= breed
+    @breed = breed
+  end
+end
