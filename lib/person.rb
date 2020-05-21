3c3caeb0b093a2031e0315443f6f729d5c96567a
Nguyen Ros <nynha0526@gmail.com>
2020-05-21 20:22:07 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+  
+  def initialize
+    @name = name
+    @job = job
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(type_of_job)
+    @job = type_of_job
+  end
+end
+
